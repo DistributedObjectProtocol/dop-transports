f0d07f41970dcc6c261bee8f9b31f8703ff9030c
Josema <enzo@josema.es>
2019-04-11 11:55:45 +0200

    Removed uws package: https://www.reddit.com/r/node/comments/91kgte/uws_has_been_deprecated/

--- listen/uws.js
+++
@@ -1,8 +0,0 @@
-// https://github.com/uWebSockets/uWebSockets
-var listenWs = require('./ws');
-function uws() {
-    return listenWs.apply(this, arguments);
-};
-
-uws.getApi = function() { return require('uws').Server };
-module.exports = uws;
