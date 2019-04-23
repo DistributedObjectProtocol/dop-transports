b85e3e591f659ad1937dc8ea5a8055d331a6fd74
Josema <enzo@josema.es>
2019-04-23 15:14:59 +0200

    Making reconnect_timeout test

--- test/reconnect_timeout.js
+++ test/reconnect_timeout.js
@@ -1,7 +1,9 @@
 const test = require('tape')
 const dop = require('dop').create()
-const dopServer = dop //.create()
-const dopClient = dop //.create()
+const dopServer = dop.create()
+const dopClient = dop.create()
+dopServer.env = 'SERVER'
+dopClient.env = 'CLIENT'
 
 const transportName = process.argv[2] || 'local'
 const transportListen = require('../').listen[transportName]
@@ -10,15 +12,14 @@ const transportConnect = require('../').connect[transportName]
 test('RECONNECTFAIL TEST', async t => {
     const server = dopServer.listen({
         transport: transportListen,
-        timeout: 5
+        timeout: 0.5
     })
     const nodeClient = await dopClient.connect({
         transport: transportConnect,
-        timeoutReconnect: 10,
+        timeoutReconnect: 1,
         listener: server
     })
-    dopServer.env = 'SERVER'
-    dopClient.env = 'CLIENT'
+
     let nodeServer
 
     server.on('connect', function(node) {
@@ -35,6 +36,17 @@ test('RECONNECTFAIL TEST', async t => {
 
     nodeClient.on('disconnect', function() {
         t.equal(nodeClient.token, nodeServer.token, 'CLIENT disconnect')
+        t.equal(
+            Object.keys(server.nodesByToken).length,
+            0,
+            'server nodesByToken 0'
+        )
+        t.equal(server.nodesBySocket.size, 0, 'server nodesBySocket 0')
+        // t.equal(
+        //     Object.keys(nodeClient.transport.nodesByToken).length,
+        //     0,
+        //     'client nodesByToken 0'
+        // )
     })
     nodeClient.on('reconnect', function(oldSocket) {
         t.equal(true, false, 'CLIENT this should not happen') // this should not happen
@@ -44,5 +56,5 @@ test('RECONNECTFAIL TEST', async t => {
     setTimeout(function() {
         // console.log( 'closing...' );
         nodeClient.socket.close()
-    }, 2500)
+    }, 250)
 })
