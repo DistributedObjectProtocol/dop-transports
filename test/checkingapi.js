a9675fee70d5bd6e6b2f9fbd1f79267d58c53948
Josema <enzo@josema.es>
2019-05-02 13:07:29 +0200

    Better tests checkingapi

--- test/checkingapi.js
+++ test/checkingapi.js
@@ -16,16 +16,29 @@ test('Api transport', function(t) {
     server = dop.listen({})
     client = dop.connect({})
     t.equal(typeof server.on, 'function')
+    t.equal(typeof server.onCreate, 'function')
+    t.equal(typeof server.onConnect, 'function')
+    t.equal(typeof server.onMessage, 'function')
+    t.equal(typeof server.onDisconnect, 'function')
+    t.equal(typeof server.onError, 'function')
     t.equal(typeof client.on, 'function')
+    t.equal(typeof client.onCreate, 'function')
+    t.equal(typeof client.onConnect, 'function')
+    t.equal(typeof client.onMessage, 'function')
+    t.equal(typeof client.onDisconnect, 'function')
+    t.equal(typeof client.onError, 'function')
+    t.equal(typeof server.socket, 'object')
     t.end()
 })
 
 test('Api node', function(t) {
     function check(node) {
         t.equal(typeof node.on, 'function')
-        t.equal(typeof node.send, 'function')
-        t.equal(typeof node.disconnect, 'function')
+        t.equal(typeof node.status, 'string')
         t.equal(typeof node.transport, 'object')
+        t.equal(typeof node.send, 'function')
+        t.equal(typeof node.subscribe, 'function')
+        t.equal(typeof node.unsubscribe, 'function')
     }
     server.on('connect', check)
     client.on('connect', function(node) {
